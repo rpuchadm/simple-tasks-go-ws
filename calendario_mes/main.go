a267996ae2de98c7066134ad8f7a95c1807a03dd
Rafael Puchades <rpuchadm@gmail.com>
2024-04-16 13:23:54 +0200

    calendar

--- calendario_mes/main.go
+++ calendario_mes/main.go
@@ -1,4 +1,3 @@
-// hello world
 package main
 
 import (
@@ -8,17 +7,19 @@ import (
 
 func main() {
 	now := time.Now()
-	lastDayOfMonth := time.Date(now.Year(), now.Month()+1, 0, 0, 0, 0, 0, time.Local)
+	firstOfMonth := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.Local)
+	lastDayOfMonth := firstOfMonth.AddDate(0, 1, -1)
 
 	fmt.Printf("%v %v\n", now.Month(), now.Year())
 
 	fmt.Println("Lu Ma Mi Ju Vi Sa Do")
-	for i := 0; i < int(now.Weekday()); i++ {
+	// Ajustamos el cálculo para el primer día de la semana
+	for i := 0; i < (int(firstOfMonth.Weekday())+6)%7; i++ {
 		fmt.Printf("   ")
 	}
 	for i := 1; i <= lastDayOfMonth.Day(); i++ {
 		fmt.Printf("%2d ", i)
-		if (i+int(now.Weekday()))%7 == 0 {
+		if (i+int(firstOfMonth.Weekday()))%7 == 0 {
 			fmt.Println()
 		}
 	}
