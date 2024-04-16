93e0c72e327db248457841c4ad95398c610785bd
Rafael Puchades <rpuchadm@gmail.com>
2024-04-16 13:49:25 +0200

    test02

--- test02/main.go
+++ test02/main.go
@@ -2,9 +2,39 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
+func generateRandomString() string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, 10)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
 func main() {
-	fmt.Println("Hello World")
+
+	randomNumber := rand.Intn(10) + 1
+	fmt.Println("Número aleatorio:", randomNumber)
+
+	if randomNumber > 5 {
+
+		stringsArray := make([]string, randomNumber)
+		for i := 0; i < randomNumber; i++ {
+			stringsArray[i] = generateRandomString()
+		}
+		fmt.Println("Strings Array:", stringsArray)
+
+	} else {
+
+		array := make([]int, randomNumber)
+		for i := 0; i < randomNumber; i++ {
+			array[i] = rand.Intn(100)
+		}
+		fmt.Println("Array:", array)
+
+	}
 
 }
